7b61c42bed44719ce378c8ab5c2532ae253535a0
katlegoJKS <katlego.malatjjie00@gmail.com>
2020-03-24 16:56:57 +0200

    hide token

--- Github_Api.py
+++ Github_Api.py
@@ -1,38 +1,24 @@
+from github import Github
 from datetime import datetime
 from datetime import timedelta, date
 
-import github
+import git_token
 
-git_client = github.Github("username", "key-here")
-pull_counts ={}
+from git_token import token
 
-def github_pull_requests(repo_name, start_date, end_date):
-    pulls = []
-    count = 0
-    # i = 1
+g = Github(token)
+
+def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-    for i in git_client.get_user().get_repos():
-        pulls.append(i.get_pulls('all'))
-        pull_counts[count] = 0
-        count += 1
-    return pulls
+start_date = date(2020, 3, 1)
+end_date = date(2020, 3, 10)
 
-github_pull_requests()
+# for single_date in daterange(start_date, end_date):
+#     date_obj = datetime.combine(single_date, datetime.min.time())
+#     print(single_date.strftime('%Y-%m-%d:'), "pull request")
 
-if __name__ == "__main__":
-    labs.github_pull_requests()
-    while True:
-        if not labs.check_counts():
-            labs.pull_counts = {}
-            labs.github_pull_requests()
-            labs.set_pull_counts()
-
-
-start_date = date(2019, 12, 1)
-end_date = date(2020, 3, 5)
-
-for single_date in daterange(start_date, end_date):
-    date_obj = datetime.combine(single_date, datetime.min.time()
-    print(single_date.strftime('%Y-%m-%d:'), "pull request")
+repos = g.get_user().get_repos()
+for repo in repos:
+    print(repo.name)
