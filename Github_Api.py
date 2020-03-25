19c6eb595429bf83be05b66faed4343bc7890803
katlegoJKS <katlego.malatjjie00@gmail.com>
2020-03-25 12:16:12 +0200

    comment-out daterange function

--- Github_Api.py
+++ Github_Api.py
@@ -8,17 +8,17 @@ from git_token import token
 
 g = Github(token)
 
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+repos = g.get_user().get_repos()
+for repo in repos:
+    print(repo.name)
 
-start_date = date(2020, 3, 1)
-end_date = date(2020, 3, 10)
+# def daterange(start_date, end_date):
+#     for n in range(int((end_date - start_date).days)):
+#         yield start_date + timedelta(n)
+
+# start_date = date(2020, 3, 1)
+# end_date = date(2020, 3, 10)
 
 # for single_date in daterange(start_date, end_date):
 #     date_obj = datetime.combine(single_date, datetime.min.time())
 #     print(single_date.strftime('%Y-%m-%d:'), "pull request")
-
-repos = g.get_user().get_repos()
-for repo in repos:
-    print(repo.name)
