afd9316859249bc4292a355d18b33d5ad5e5544d
katlegoJKS <katlego.malatjie@umuzi.org>
2020-06-02 18:03:02 +0200

    updated

--- Github_Api.py
+++ Github_Api.py
@@ -2,16 +2,14 @@ import requests
 
 from datetime import timedelta, date
 
-
 def pull_requests():
       git_info = {
-        'created_at':2020-1-2,
-        'updated_at': 2020-4-21,
+        'created_at':2020-4-2,
+        'updated_at': 2020-5-21,
         'pushed_at': 2020-4-21
       }
 
       ghpull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=1", data=git_info)
-      user_data = ghpull_requests.json()
-      print(user_data)
+      print(ghpull_requests.json())
 
 pull_requests()
