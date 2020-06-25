9eb54ab23e12c9d18bf22b45bf114ba60facc1dc
katlegoJKS <katlego.malatjie@umuzi.org>
2020-06-25 12:50:20 +0200

    Updated API function

--- Github_Api.py
+++ Github_Api.py
@@ -2,14 +2,18 @@ import requests
 
 from datetime import timedelta, date
 
-def pull_requests():
+def pull_requests(repository_name, start_date, end_date):
       git_info = {
-        'created_at':2020-4-2,
-        'updated_at': 2020-5-21,
-        'pushed_at': 2020-4-21
-      }
+        'created_at':2020-4-1,
+        'updated_at': 2020-6-20,
+        'merged_at': {
+            'start_ddate': start_date,
+            'end_date': end_date
+            }
+        }
 
-      ghpull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=1", data=git_info)
-      print(ghpull_requests.json())
+      repo_pull_requests = requests.get(repository_name, data=git_info)
+      return repo_pull_requests.json()
 
-pull_requests()
+pull_requests('https://api.github.com/repos/Umuzi-org/tech-department/pulls', 2020-4-1, 2020-6-1)
+
